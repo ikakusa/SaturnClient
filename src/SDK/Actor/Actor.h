3b91c8c5e5ea78dccf692648470f14a1a6410d9e
誠 遺憾 <ikasan2860@gmail.com>
2026-02-03 15:40:25 +0900

    aaaa

--- src/SDK/Actor/Actor.h
+++ src/SDK/Actor/Actor.h
@@ -2,8 +2,10 @@
 #include <MemUtils.h>
 class Level;
 struct StateVectorComponent;
+struct RotationComponent;
 class Actor {
 public:
 	SAFE_ACCESS(StateVectorComponent*, stateVector, 0x218); //1.21.132
+	SAFE_ACCESS(RotationComponent*, rotation, 0x228); //1.21.132
 	SAFE_ACCESS(Level*, level, 0x1D8); //1.21.132
 };
